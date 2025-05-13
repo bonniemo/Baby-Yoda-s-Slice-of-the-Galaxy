8b47c642d987d26c0e9547dd305e34973e103a73
bonniemo <moller.bon@gmail.com>
2025-05-13 08:18:07 +0200

    styling adjustments

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,21 +1,28 @@
-import "./Hero.scss";
 import yoda from "../../Images/pizzayoda.png";
+import "./Hero.scss";
 
 const Hero = () => {
-  return (
-    <>
-      <article className="hero" id="hero">
-        {/* <img src={starBg} alt="" className="star-bg" /> */}
-        <section className="heading">
-          <h2>
-            <span>Embark on a Galactic Culinary Adventure with</span>
-          </h2>
-          <h1>Baby Yoda's Slice <span>of the Galaxy</span></h1>
-          <h2>
-            Experience the warp-speed delivery of our <span>out-of-this-world pizzas,
-            guided by the spirit of the Force.</span>
-          </h2>
-          {/*
+    return (
+        <>
+            <article className="hero" id="hero">
+                {/* <img src={starBg} alt="" className="star-bg" /> */}
+                <section className="heading">
+                    <h2>
+                        <span>
+                            Embark on a Galactic Culinary Adventure with
+                        </span>
+                    </h2>
+                    <h1>
+                        Baby Yoda's Slice <span>of the Galaxy</span>
+                    </h1>
+                    <p>
+                        Experience the warp-speed delivery of our{" "}
+                        <span>
+                            out-of-this-world pizzas, guided by the spirit of
+                            the Force.
+                        </span>
+                    </p>
+                    {/*
           
           As Master Yoda once said,
           <span>"'Deliver. Or deliver not. There is no delay."</span>
@@ -25,13 +32,13 @@ const Hero = () => {
             bite. 
           </p>
             */}
-        </section>
-        <section className="yoda-wrap">
-          <img src={yoda} alt="" />
-        </section>
-      </article>
-    </>
-  );
+                </section>
+                <section className="yoda-wrap">
+                    <img src={yoda} alt="" />
+                </section>
+            </article>
+        </>
+    );
 };
 
 export default Hero;
